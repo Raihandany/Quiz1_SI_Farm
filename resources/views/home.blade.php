01da8aa35385a2a9913a821f9c9e078fcd742920
Raihandany <raihandaniy@gmail.com>
2023-03-19 12:47:06 +0700

    steps2new

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="public/style.css">
         <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
         <title>Dashboard || Rainsel'Bag</title>
     </head>
