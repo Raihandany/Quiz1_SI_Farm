7d17fdb947a02301ee50ee21e18d26fc17bc45e0
Raihandany <raihandaniy@gmail.com>
2023-03-19 12:44:37 +0700

    steps2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,48 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Feature | Home</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        <style>
-        
-        </style>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+        <title>Dashboard || Rainsel'Bag</title>
     </head>
 
     <body>
+        <!-- header --!>
+        <header>
+            <div class="container">
+                <h1><a href="dashboard.php">SI | FARM</a></h1>
+                <ul>
+                    <li><a href="dashboard.php">Lokation</a></li>
+                    <li><a href="">Transaction</a></li>
+                    <li><a href="">Facility</a></li>
+                    <li><a href="">Data Actort</a></li>   
+                    <li><a href="">Exit</a></li>
+                </ul>
+            </div>
+        </header>
+
+        <!-- content --!>
+        <div class="section">
+        <div class="container">
+            <h3>Facility</h3>
+            <div class="box">
+                <p><a href="request-add.php">Request</a></p>
+                <table border = "3" cellspacing="0" class="table">
+                    <thead>
+                        <tr>
+                            <th width="60px">No</th>
+                            <th>Name User</th>
+                            <th>Request Goods</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    
+
+
+                </table>
+            </div>
+        </div>
+    </div>
 
     <body>
 
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    echo 'welcome evan fadilah & raihandany';
-    echo 'tes';
+    return view('home');
 
 });
